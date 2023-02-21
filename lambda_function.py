1d0f102c2f8936f162734c68cf2c79525d33cd2e
Natalia Fekonja <64786815+nataliafkj@users.noreply.github.com>
2023-02-20 22:07:23 -0300

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -15,38 +15,26 @@ def lambda_handler(event, context):
     aws_access_key_id = os.environ['aws_access_key_id']
     aws_secret_access_key = os.environ['aws_secret_access_key']
     
-    img_url = 'link'
-    
-    response = requests.get(img_url)
-    
-    img = BytesIO(response.content)
-    
-    s3 = boto3.client('s3', 
-        aws_access_key_id=aws_access_key_id, 
-        aws_secret_access_key=aws_secret_access_key)
-      
     bucket_name = "nayeonbucket"
-    
-    image_numb = random.randint(1, 731)
-    
-    if random.randint(1, 2) == 2 & image_numb > 602:
-        name_image = "%s%s" % (image_numb, ".mp4")
-        name_temp = "/tmp/%s" % (name_image)
-        
-    else:
-        name_image = "%s%s" % (image_numb, ".jpg")
-        name_temp = "/tmp/%s" % (name_image)
-    
     
     s3_resource = boto3.resource('s3', 
         aws_access_key_id=aws_access_key_id, 
         aws_secret_access_key=aws_secret_access_key)
-        
-
-    s3_resource.Bucket('nayeonbucket').download_file(name_image, name_temp)
+    
+    #Create image list
+    my_bucket = s3_resource.Bucket('nayeonbucket')
+    a = [my_bucket_object.key for my_bucket_object in my_bucket.objects.all()] 
+    
+    #Choose a photo
+    image_numb = random.randint(0, len(a))
+    
+    photo_name = a[image_numb]
+    
+    name_temp = "/tmp/%s" % (photo_name)
     
     status = '#나연 #NAYEON #TWICE'
     
+    s3_resource.Bucket('nayeonbucket').download_file(photo_name, name_temp)
     
     #Authenticating
     auth = tweepy.OAuthHandler(api_key, api_secret)
@@ -57,3 +45,9 @@ def lambda_handler(event, context):
     
     #Create a tweet
     api.update_with_media(name_temp, status)
+    
+    #Delete image posted
+    s3 = boto3.client('s3', 
+        aws_access_key_id = aws_access_key_id, 
+        aws_secret_access_key = aws_secret_access_key)
+    s3.delete_object(Bucket='nayeonbucket', Key=photo_name)
